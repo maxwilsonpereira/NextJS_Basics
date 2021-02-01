1064ed5f051c30f958bdd6416f3cf6fa3cdc07d9
“maxwilsonpereira <maxwilsonpereira@gmail.com>
2021-02-01 13:37:47 +0100

    second commit

--- pages/_BASICS/fileSystem.tsx
+++
@@ -1,9 +0,0 @@
-// FROM pages/dynamic-render
-
-import path from "path";
-
-const fs = require("fs");
-const listTxt = fs.readFileSync(
-  path.join(process.cwd(), "pages/dynamic-render/list.txt"),
-  "utf8"
-);
--- pages/_BASICS/typeScript.tsx
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-
-export default function TypeScript() {
-  const [username, setUsername] = useState<string>("");
-  return null;
-}
